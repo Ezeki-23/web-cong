831b552edfbacd4d0b627d01c72066a6d01129ae
Laureano Ezequiel Alvarez <137580645+Ezeki-23@users.noreply.github.com>
2023-10-03 23:09:40 -0300

    Update images.js

--- src/components/assets/images/images.js
+++ src/components/assets/images/images.js
@@ -1,38 +1,30 @@
-import img_2 from '../images/img_2.jpg'
-import img_3 from '../images/Img_3.jpg'
-import img_4 from '../images/Img_4.jpg'
-import img_5 from '../images/Img_5.jpg'
-import img_6 from '../images/Img_6.jpg'
-import img_7 from '../images/Img_7.jpg'
-import img_8 from '../images/Img_8.png'
-
 export const images = [
     {
-        name: img_2,
+        name: 'https://i.postimg.cc/QNPZkMZH/Img2.jpg',
         id: '1',
     },
     {
-        name: img_3,
+        name: 'https://i.postimg.cc/L5rdHx3z/Img-3.jpg',
         id: '2',
     },
     {
-        name: img_4,
+        name: 'https://i.postimg.cc/tCxQrH9D/Img-4.jpg',
         id: '3',
     },
     {
-        name: img_5,
+        name: 'https://i.postimg.cc/sgCFq0Xz/Img-5.jpg',
         id: '4',
     },
     {
-        name: img_6,
+        name: 'https://i.postimg.cc/cHkyzxKk/Img-6.jpg',
         id: '5',
     },
     {
-        name: img_7,
+        name: 'https://i.postimg.cc/7hC88F0H/Img-7.jpg',
         id: '6',
     },
     {
-        name: img_8,
+        name: 'https://i.postimg.cc/GmP0CJ8y/img-8.png',
         id: '7',
     },
 ]
