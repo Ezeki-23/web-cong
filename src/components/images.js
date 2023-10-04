45977627c2a8166a06089468ae730eba4ddab59a
Laureano Ezequiel Alvarez <137580645+Ezeki-23@users.noreply.github.com>
2023-10-03 23:12:00 -0300

    Create images.js

---
+++ src/components/images.js
@@ -0,0 +1,30 @@
+export const images = [
+    {
+        name: 'https://i.postimg.cc/QNPZkMZH/Img2.jpg',
+        id: '1',
+    },
+    {
+        name: 'https://i.postimg.cc/L5rdHx3z/Img-3.jpg',
+        id: '2',
+    },
+    {
+        name: 'https://i.postimg.cc/tCxQrH9D/Img-4.jpg',
+        id: '3',
+    },
+    {
+        name: 'https://i.postimg.cc/sgCFq0Xz/Img-5.jpg',
+        id: '4',
+    },
+    {
+        name: 'https://i.postimg.cc/cHkyzxKk/Img-6.jpg',
+        id: '5',
+    },
+    {
+        name: 'https://i.postimg.cc/7hC88F0H/Img-7.jpg',
+        id: '6',
+    },
+    {
+        name: 'https://i.postimg.cc/GmP0CJ8y/img-8.png',
+        id: '7',
+    },
+]
