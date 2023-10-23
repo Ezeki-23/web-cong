e3a7efa6aba8bd12cbf987f791fe71ded133783b
Laureano Ezequiel Alvarez <137580645+Ezeki-23@users.noreply.github.com>
2023-10-22 23:35:18 -0300

    Update Services.jsx

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,3 +1,6 @@
+import ItemsContainer from "../components/Footer/ItemsContainer";
+import SocialIcons from "../components/Footer/SocialIcons";
+import { Icons } from "../components/Footer/Menus";
 
 function Services ()  {
     return (
@@ -114,6 +117,25 @@ function Services ()  {
         </div>
         </div>
     </section>
+    <footer className="bg-orange-900 text-white">
+      <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#ffffff19] py-7">
+        <h1
+          className="md:text-4xl text-xl md:mb-0 mb-6 lg:leading-normal font-semibold
+         md:w-2/5"
+        >
+          <span className=" text-yellow-300">¡Gracias</span> por pasar por aquí!
+        </h1>
+      </div>
+      <ItemsContainer />
+      <div
+        className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10
+      text-center pt-2 text-gray-400 text-sm pb-8"
+      >
+        <span>© 2023 Congregación Nueva Esperanza. All rights reserved.</span>
+        <span>Terms · Privacy Policy</span>
+        <SocialIcons Icons={Icons} />
+      </div>
+    </footer>
     </div>
   )
 }
